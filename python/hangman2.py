d5e9443a719a823e9883bdb8c92398499dd75a91
1337Athenachan <1337Athena@proton.me>
2024-05-25 05:45:07 -0400

    python work, changes to hangman2 script

--- python/hangman2.py
+++ python/hangman2.py
@@ -2,7 +2,7 @@
 
 import random
 
-# Cretae greeting
+# Create greeting
 print("welcome to my hangman game!")
 
 # Create word list
@@ -11,31 +11,38 @@ words = ["cyber", "security", "tester"]
 # Randomly choose word
 secret_choice = random.choice(words)
 
+
 # Testing purposes
 #print(secret_choice)
 
 # Creating a list to store our symbols
 display_word = []
+
 for letter in secret_choice:
-    display_word += "_"
-print(display_word)
+	display_word += "_"
+print(display_word)	
+	
+# Game end condition		
+game_over = False
 
-# Adding our game into a while loop to keep it running until the user has finished the game
-game_over =False
-
+# Add while loop to finish the game 
 while not game_over:
+
 # Player input
-    guess = input("Guess a letter?: " ).lower()
-# For loop to iterate the letters
-# It will print correct for every choice you get right
-    for position in range(len(secret_choice)):
-        letter = secret_choice[position]
-        if letter == guess:
-            display_word[position] = letter
-            
-    print(display_word)
-    if "_" not in display_word:
-        print("You win!")
-        game_over = True  
+	guess = input("Please Enter your guess: ")	
+
+# For loop to iterate range len on position of secret choice
+	for position in range(len(secret_choice)):
+#		print(position)
+		letter = secret_choice[position]
+#		print(letter)
+		if letter == guess:
+			display_word[position] = letter
+	print(display_word)
+	
+# print display and end game			
+	if "_" not in display_word:
+		print("You Win!")
+		game_over = True
 
 
