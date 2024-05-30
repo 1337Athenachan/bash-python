ae42451c88d46bf710cc83927f3c6fed5c1a83ba
1337Athenachan <1337Athena@proton.me>
2024-05-30 05:51:57 -0400

    Python programs: added more completed excersises, fav-number and number-eight

---
+++ python/crash-course-second-ed/fav-number-2-9.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+# Task: Store your favorite number in a variable Then, using that variable, create a message that reveals your favorite number Print that message
+
+fav_num = 13
+message = f"my favourite number is {fav_num}"
+print(message)
