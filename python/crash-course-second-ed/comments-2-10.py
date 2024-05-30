f48d601f0b2aa81c9ae0b0f057d473918b6ccc11
1337Athenachan <1337Athena@proton.me>
2024-05-30 06:01:43 -0400

    Python programs: added more completed excersises, adding comments to code

---
+++ python/crash-course-second-ed/comments-2-10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+# Task: Choose two of the programs you’ve written, and add at least one comment to each If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file Then write one sentence describing what the program does
+
+# Making a simple program to show comments
+
+# Ask for user for theri name 
+user_name = input("What is your user name please?: ")
+greeting = f"Hello {user_name}, thank you for using this small program"
+
+# Print username and greeting
+print(greeting)
+
